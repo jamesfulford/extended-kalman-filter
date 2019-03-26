f063cd876499e9e7dd82c7f011ea1d6a393e1c7f
James Fulford <james.fulford@outlook.com>
2019-03-25 23:35:59 -0400

    WIP(fusion): Predict/Update logic

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -80,32 +80,45 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
    * Prediction
    */
 
-  /**
-   * TODO: Update the state transition matrix F according to the new elapsed time.
-   * Time is measured in seconds.
-   * TODO: Update the process noise covariance matrix.
-   * Use noise_ax = 9 and noise_ay = 9 for your Q matrix.
-   */
+	// compute the time elapsed between the current and previous measurements
+	// dt - expressed in seconds
+	float noise_ax = 9;
+	float noise_ay = 9;
 
-  ekf_.Predict();
+	float dt = (measurement_pack.timestamp_ - previous_timestamp_) / 1000000.0;
+	previous_timestamp_ = measurement_pack.timestamp_;
+
+	float dt2 = dt * dt;
+	float dt3 = dt2 * dt;
+	float dt4 = dt3 * dt;
+
+	ekf_.F_ = MatrixXd(4, 4);
+	ekf_.F_ << 1, 0, dt, 0,
+						 0, 1, 0, dt,
+						 0, 0, 1, 0,
+						 0, 0, 0, 1;
+
+	ekf_.Q_ = MatrixXd(4, 4);
+	ekf_.Q_ << dt4 * noise_ax / 4, 0, dt3 * noise_ax / 2, 0,
+						 0, dt4 * noise_ay / 4, 0, dt3 * noise_ay / 2,
+						 dt3 * noise_ax / 2, 0, dt2 * noise_ax, 0,
+						 0, dt3 * noise_ay / 2, 0, dt2 * noise_ay;
+
+	ekf_.Predict();
 
   /**
    * Update
    */
 
-  /**
-   * TODO:
-   * - Use the sensor type to perform the update step.
-   * - Update the state and covariance matrices.
-   */
-
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
-    // TODO: Radar updates
-
-  } else {
-    // TODO: Laser updates
-
-  }
+		ekf_.R_ = R_radar_;
+		ekf_.H_ = H_laser_;
+		ekf_.Update(measurement_pack.raw_measurements_);
+	} else {
+		ekf_.R_ = R_laser_;
+		ekf_.H_  = tools.CalculateJacobian(ekf_.x_); // TODO(jamesfulford): Use current state or measurement?
+		ekf_.UpdateEKF(measurement_pack.raw_measurements_);
+	}
 
   // print the output
   cout << "x_ = " << ekf_.x_ << endl;
