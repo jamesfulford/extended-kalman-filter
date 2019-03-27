099a1280bddf5a0b301bd2be8dd1a259896b70df
James Fulford <james.fulford@outlook.com>
2019-03-26 21:14:02 -0400

    WIP(fusion): Fixing update step mis-match

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -128,11 +128,11 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     ekf_.R_ = R_radar_;
-    ekf_.H_ = H_laser_;
+    ekf_.H_ = tools.CalculateJacobian(ekf_.x_);  // TODO(jamesfulford): Use current state or measurement?
     ekf_.Update(measurement_pack.raw_measurements_);
-  } else {
+  } else { // Laser
     ekf_.R_ = R_laser_;
-    ekf_.H_  = tools.CalculateJacobian(ekf_.x_); // TODO(jamesfulford): Use current state or measurement?
+    ekf_.H_  = H_laser_;
     ekf_.UpdateEKF(measurement_pack.raw_measurements_);
   }
 
