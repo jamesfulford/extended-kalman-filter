43fdc831445057c0c70015ab68ccb2cd1c5ea220
James Fulford <james.fulford@outlook.com>
2019-04-01 23:42:52 -0400

    fix(tools): Removing compile error due to merge issue

--- src/tools.cpp
+++ src/tools.cpp
@@ -31,9 +31,9 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
-  MatrixXd Hj(3, 4);
-  // recover state parameters
   MatrixXd Hj = MatrixXd(3, 4);
+
+  // recover state parameters
   float px = x_state(0);
   float py = x_state(1);
   float vx = x_state(2);
