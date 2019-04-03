ee5cafa46eaf0f18261427a34b7cb5798b80dae3
James Fulford <james.fulford@outlook.com>
2019-04-02 20:20:08 -0400

    feat(build): Adding commands to help with building and running

---
+++ build.sh
@@ -0,0 +1,8 @@
+# To install dependencies in dev environment
+cd /home/workspace/CarND-Extended-Kalman-Filter-Project && ./install-ubuntu.sh
+
+# To compile and run in dev environment
+cd /home/workspace/CarND-Extended-Kalman-Filter-Project/build && cmake .. && make && ./ExtendedKF
+
+# To run in GPU environment:
+cd /home/workspace/CarND-Extended-Kalman-Filter-Project/build && ./ExtendedKF
